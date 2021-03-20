523a4d20c7d4dfa419ad9f92e3df9277c30ee38c
calebkw <walke1291@gmail.com>
2021-03-20 10:56:59 -0500

    fixed initial page redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
@@ -10,9 +10,12 @@ function App() {
         <Router>
             <div>
                 <NavTabs />
-                <Route exact path="/" component={Home} />
+                <Route exact path="/">
+                    <Redirect to="/home" />
+                </Route> 
+                <Route exact path="/home" component={Home} />
                 <Route exact path="/projects" component={Projects} />
-                <Route path="/contact" component={Contact} />
+                <Route exact path="/contact" component={Contact} />
             </div>
         </Router>
     );
